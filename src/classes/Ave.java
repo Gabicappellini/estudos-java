3e5462737c077aacd4562503320234a7eb58d3b5
Browie <arketing@letscode.com.br>
2023-11-08 22:10:56 -0300

    removendo linhas em branco

--- src/classes/Ave.java
+++ src/classes/Ave.java
@@ -2,13 +2,9 @@ package classes;
 
 public class Ave {
     public String raca;
-
     public String especie;
-
     public String cor;
-
     public boolean sabeVoar;
-
     public boolean temHabitosNoturnos;
 
     public void seApresentar() {
